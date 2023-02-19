32e7c2a4ee80d23e030efdd5bd746ff3c20187f3
weixuan18 <weixuan.cheng@outlook.com>
2023-02-19 23:32:49 +0100

    version_finale

--- extraire_lexique.py
+++ extraire_lexique.py
@@ -1,17 +1,59 @@
+from pathlib import Path
 from typing import List, Dict
 
-def nb_doc(list_chaine: List[str]) -> Dict[str, int]:
-    dic={}
+def lire_corpus():
+    corpus_dir = Path("./Corpus")
+    resultat = []
+    for fichier in corpus_dir.iterdir():
+        texte = fichier.read_text("utf-8")
+        resultat.append(texte)
+    return resultat
 
-    for corpus in liste_chaine:
+def term_freq(corpus: List[str]) -> Dict[str,int]:
+    resultat = {}
+    for doc in corpus:
+        for word in doc.split():
+            if word in resultat:
+                resultat[word] += 1
+            else:
+                resultat[word] = 1
+    return resultat
+
+def doc_freq(corpus: List[str]) -> Dict[str,int]:
+    resultat = {}
+    for doc in corpus:
+        words = set(doc.split())
+        for word in words:
+            if word in resultat:
+                resultat[word] += 1
+            else:
+                resultat[word] = 1
+    return resultat
+
+def nb_doc(corpus: List[str]) -> Dict[str, int]:
+    resultat={}
+
+    for doc in corpus:
         texte = set(corpus.split())
 
         for mot in corpus:
             if mot in dic:
-                dic[mot] += 1
+                resultat[mot] += 1
             else:
-                dic[mot] = 1
-return dic
+                resultat[mot] = 1
+    return resultat
 
+def main():
+    corpus = lire_corpus()
+    print("doc freq")
+    for k, v in doc_freq(corpus).items():
+        print(f"{k}: {v}")
+    print("term freq")
+    for k, v in term_freq(corpus).items():
+        print(f"{k}: {v}")
+
+
+if __name__ == "__main__":
+    main()
 
 
