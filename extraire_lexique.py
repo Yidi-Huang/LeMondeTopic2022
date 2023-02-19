b47e605e5139482ece6690c4504d54e83414afff
XINHAO_ZHANG <zhangxinhao672@gmail.com>
2023-02-19 17:22:48 +0100

    essai de role 1 pour exercice

--- extraire_lexique.py
+++ extraire_lexique.py
@@ -1,10 +1,22 @@
 from pathlib import Path
 from typing import List, Dict
+import argparse
+import sys
+import glob
+
+parser = argparse.ArgumentParser(description = 'lire le corpus du dossier')
+parser.add_argument('path', nargs='+', type=str, required = True, help='nom du dossier du chemin que vous souhaitez lire')
+args = parser.parse_args()
+
+
+def lecture_par_liste():
+    # Cela renvoie une liste contenant les noms de tous les fichiers avec l'extension .txt dans le dossier en argument.
+    liste_fichiers = glob.glob(sys.argv[1]  + "/*.txt")
+    return liste_fichiers
 
 def lire_corpus():
-    corpus_dir = Path("./Corpus")
     resultat = []
-    for fichier in corpus_dir.iterdir():
+    for fichier in liste_fichiers:
         texte = fichier.read_text("utf-8")
         resultat.append(texte)
     return resultat
@@ -31,7 +43,7 @@ def doc_freq(corpus: List[str]) -> Dict[str,int]:
     return resultat
 
 def main():
-    corpus = lire_corpus()
+    corpus = lecture_par_liste(lire_corpus())
     print("doc freq")
     for k, v in doc_freq(corpus).items():
         print(f"{k}: {v}")
@@ -39,6 +51,8 @@ def main():
     for k, v in term_freq(corpus).items():
         print(f"{k}: {v}")
 
+if __name__ == '__main__':
+    print main(args.path)
 
-if __name__ == "__main__":
-    main()
+
+
