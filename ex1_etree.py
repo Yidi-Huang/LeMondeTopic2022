7d969afc6fa602c789be4b55907b0970a2928f5e
yidi <yidi.huang.elodie@gmail.com>
2023-03-15 19:01:47 +0100

    update_sys

--- ex1_etree.py
+++ ex1_etree.py
@@ -1,19 +1,20 @@
-import sys
-import xml.etree.ElementTree as ET
+def extraire_td(filename):
+    import sys
+    import xml.etree.ElementTree as ET
 
-# Obtenir le nom de fichier XML depuis la ligne de commande
-filename = sys.argv[1]
+    # le nom de fichier XML depuis le terminal
+    filename = sys.argv[1]
 
-# Parser le fichier XML
-tree = ET.parse(filename)
-root = tree.getroot()
 
-# Parcourir les éléments et extraire les titres et les descriptions
-for item in root.findall(".//item"):
-    title = item.find("title").text
-    description = item.find("description").text
+    tree = ET.parse(filename)
+    root = tree.getroot()
 
-    # Afficher le titre et la description de chaque article
-    print(f"{title}")
-    print(f"{description}\n")
+    # Parcourir les éléments et extraire titres et descriptions
+    for item in root.findall(".//item"):
+        title = item.find("title").text
+        description = item.find("description").text
+
+
+        print(f"{title}")
+        print(f"{description}\n")
 
