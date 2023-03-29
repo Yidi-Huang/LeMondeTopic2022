df4c378c48642ac9730d52015f2338e8e0a581cf
yidi <yidi.huang.elodie@gmail.com>
2023-03-29 21:03:10 +0200

    remplace_yield

--- ex1_etree.py
+++ ex1_etree.py
@@ -1,20 +1,13 @@
 def extraire_td(filename):
-    import sys
     import xml.etree.ElementTree as ET
-
-    # le nom de fichier XML depuis le terminal
-    filename = sys.argv[1]
-
-
     tree = ET.parse(filename)
     root = tree.getroot()
-
-    # Parcourir les éléments et extraire titres et descriptions
     for item in root.findall(".//item"):
         title = item.find("title").text
         description = item.find("description").text
-
-
-        print(f"{title}")
-        print(f"{description}\n")
-
+        yield title, description
+        
+import sys
+filename = sys.argv[1]
+for t, d in extraire_td(filename):
+    print(t, d)
