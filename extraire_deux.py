71eb2381517381fd48055a059d9a194f2c4007a7
weixuan18 <weixuan.cheng@outlook.com>
2023-04-11 13:34:12 +0200

    erreur_commit

--- extraire_deux.py
+++ extraire_deux.py
@@ -1,11 +1,13 @@
 import xml.etree.ElementTree as ET
+import datetime
 import feedparser
 import pathlib
+import sys
 import argparse
 from pathlib import Path
 
 # Définir le chemin du dossier contenant les fichiers XML
-xml_folder = pathlib.Path("/home/weixuan/文档/M1S8-PPE2/Corpus/X-arborescence-filsdumonde-2022-tljours-19h/2022")
+xml_folder = pathlib.Path("/home/zhang/文档/2022")
 
 # Définir le dictionnaire de correspondance entre les catégories et les noms de fichiers XML
 categories_dict = {
@@ -29,12 +31,11 @@ categories_dict = {
 new_dict = {valeur: cle for cle, valeur in categories_dict.items()}
 
 
-def extraire_par_date():
-    # Demander la date à rechercher
-    month = input("Entrez le mois (en lettres, par exemple : Jan, Feb, MAR (Veuillez saisir les trois premiers lettres)etc.) : ")
-    day = input("Entrez le jour (sous la forme 01, 02, ..., 31) : ")
+def extraire_par_date(xml_folder, month,day):
+   
     # Vérifier si le dossier existe
     date_dir = xml_folder / month / day
+    dates=[]
     if not date_dir.is_dir():
         print("Le dossier pour cette date n'existe pas.")
     else:
@@ -74,6 +75,7 @@ def extraire_par_date():
     print(f"Le fichier XML pour la date '{month}{day}' a été créé avec succès.") 
 
 def extraire_par_categorie():
+    dates=[]
     # Demander la catégorie à rechercher
     category = input("Entrez la catégorie (une, international, europe) : ")
 
@@ -96,7 +98,20 @@ def extraire_par_categorie():
                 for hour_dir in day_dir.iterdir():
                     if not hour_dir.is_dir():
                         continue
-                    
+
+                    month = datetime.datetime.strptime(month_dir.name, '%b')
+                    day = datetime.datetime.strptime(day_dir.name, '%d')
+                    date = datetime.datetime(month.year, month.month, day.day)
+                    dates.append(date)
+
+                    # trier les dates en ordre croissant
+                    sorted_dates = sorted(dates)
+
+                    for date in sorted_dates:
+                        month = date.strftime('%b')
+                        day = date.strftime('%d')
+                        extraire_par_date(month, day)
+
                     xml_path = hour_dir / f"{categories_dict[category]}.xml"                    
                     if xml_path.exists():
                         date_elem =ET.Element("date")
@@ -133,6 +148,7 @@ def main():
         extraire_par_categorie()
     else:
         parser.print_help()
+        
 
 if __name__ == "__main__":
     main()
